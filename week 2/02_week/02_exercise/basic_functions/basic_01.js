de42d1b045f3082e6fcf1d79c5984c9fe7466854
Anwar Sayeed <s2400057@BC-C02FX17RQ6LTs-MacBook-Air.local>
2024-08-21 12:25:41 +0300

    week2 added

---
+++ week 2/02_week/02_exercise/basic_functions/basic_01.js
@@ -0,0 +1,10 @@
+// Write the function sum such that it returns the sum of a and b.
+
+function add(a,b){
+    return a+b;
+}
+
+
+// sample usage - do not modify
+console.log(sum(1, 3, 5));
+console.log(sum(2, 5));
